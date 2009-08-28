3f6edb77f0bd7bbd3a49186ea565d95e5e29805d
Łukasz Piestrzeniewicz <bragi@ragnarson.com>
2009-08-28 15:56:14 +0200

    Documented execution flow

--- LExecution.m
+++ LExecution.m
@@ -44,15 +44,20 @@
     target = context;
     message = nmessage;
     while (message) {
-        // TODO: Rethink. Maybe we should do message runWithExecution and message decides what to do next
-        // Normal messages re-send to target. Literals create instances. Complex.
+        // TODO: Rethink. Maybe we should call message's runWithExecution and message decides what to do next
+        // Normal messages re-send to target. Literals create instances. Complex...
+        
+        // Handle literals
         if([message isKindOfClass: [LLiteral class]]) {
+            // For each literal type create proper object and make it a new target
             if([message isKindOfClass:[LTextLiteral class]]) {
                 target = [runtime makeTextWithString:message.name];
             }
         } else if([message isKindOfClass: [EndMessage class]]) {
+            // Handle EndMessage: This messages resets current target back to original context
             target = context;
         } else {
+            // End of special cases: send the message to current target, set target to what methods return.
             target = [target send:self];
         }
         message = message.nextMessage;
