2ddeabe162ce708e1d709158a69e0c723ca6279c
Łukasz Piestrzeniewicz <bragi@ragnarson.com>
2009-09-07 19:44:00 +0200

    Added BaseContext cell to BaseContext

--- RBaseContext.m
+++ RBaseContext.m
@@ -13,17 +13,29 @@
 
 + (void) addCellsTo: (LObject*)object inRuntime:(LRuntime*)runtime
 {
-    /* Add methods */
+    /* Self in BaseContext */
+	[object setCell:object withName:@"BaseContext"];
+
+    /* Basic methods */
 	[object setCell:[[SelfMethod alloc] init] withName:@"self"];
 	[object setCell:[[ForwardingMethod alloc] initWithName:@"assignment"] withName:@"="];
 	[object setCell:[[MethodMethod alloc] init] withName:@"method"];
+    
+    /* Object cell */
 	[object setCell:runtime.theObject withName:@"Object"];
+    
+    /* True cell */
 	[object setCell:[[TrueMethod alloc] init] withName:@"true"];
 	[object setCell:[[TrueMethod alloc] init] withName:@"True"];
+
+    /* False cell */
 	[object setCell:[[FalseMethod alloc] init] withName:@"false"];
 	[object setCell:[[FalseMethod alloc] init] withName:@"False"];
+
+    /* Nil cell */
 	[object setCell:[[NilMethod alloc] init] withName:@"nil"];
 	[object setCell:[[NilMethod alloc] init] withName:@"Nil"];
+
 }
 
 @end
